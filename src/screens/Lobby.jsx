57194bd8977fc9cb8e18f0128e265d59b74e501e
GenanawMekete <104999951+GenanawMekete@users.noreply.github.com>
2026-01-22 20:14:10 +0300

    Update Lobby.jsx

--- src/screens/Lobby.jsx
+++ src/screens/Lobby.jsx
@@ -1,20 +1,30 @@
 import { useEffect, useState } from "react";
+import { socket } from "../socket";
 
-export default function Lobby({ onStart }) {
+export default function Lobby({ user, roomId, onStart }) {
   const [time, setTime] = useState(30);
+  const [players, setPlayers] = useState([]);
 
   useEffect(() => {
-    const t = setInterval(() => setTime(x => x - 1), 1000);
-    if (time === 0) onStart();
-    return () => clearInterval(t);
+    socket.emit("join-game", { telegramId: user.telegramId, roomId });
+
+    socket.on("update-players", setPlayers);
+
+    const interval = setInterval(() => setTime(t => t - 1), 1000);
+    if (time <= 0) onStart();
+
+    return () => {
+      clearInterval(interval);
+      socket.off("update-players");
+    };
   }, [time]);
 
   return (
-    <div className="box">
-      <h2>Waiting for players…</h2>
-      <p>Game starts in {time}s</p>
-      <p>Bet: 10 ETB</p>
-      <p>Derash: 80%</p>
+    <div className="winner-box">
+      <h2>Lobby: Room {roomId}</h2>
+      <p>Players joined: {players.length}</p>
+      <p>Game starts in: {time}s</p>
+      <p>Bet: 10 Birr | First-time bonus included</p>
     </div>
   );
 }
